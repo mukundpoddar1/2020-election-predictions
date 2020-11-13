1d428d3b0a4adac7b4f010aeb53e5f334db22536
nponarul <nponarul@gmail.com>
2020-11-12 21:01:08 -0500

    Added crosswalk from county to congressional district

---
+++ scripts/cleaning_scripts/county_to_116congress_clean.R
@@ -0,0 +1,30 @@
+#########################
+# Title: 260 Project 
+# Purpose: Download and process county to 116th congressional district crosswalk 
+#########################
+
+
+
+# SETUP -------------------------------------------------------------------
+
+rm(list = ls())
+
+gc(reset = TRUE)
+
+library(tidyverse)
+library(ggplot2)
+
+
+# DOWNLOAD DATA -----------------------------------------------------------
+
+
+cw <- read.table("https://www2.census.gov/geo/relfiles/cdsld18/natl/natl_cocd_delim.txt", skip = 2, colClasses = "character")
+
+
+cw <- cw %>% 
+  separate(V1, into = c("state", "county","congress_district"), sep = ",")
+
+
+# OUTPUT ------------------------------------------------------------------
+
+saveRDS(cw, "~/Documents/2020-election-predictions/data/county_to_116th_congress.rds")
