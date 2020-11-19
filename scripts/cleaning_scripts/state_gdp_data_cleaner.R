987b86f8d186936ef9c7d61fdf510f99b9c0ab07
Mukund Poddar <mukundpoddar1@gmail.com>
2020-11-19 23:26:37 +0530

    Change file paths in some scripts and add state code to base state-county file

--- scripts/cleaning_scripts/state_gdp_data_cleaner.R
+++ scripts/cleaning_scripts/state_gdp_data_cleaner.R
@@ -9,7 +9,7 @@
 
 library(tidyverse)
 
-state_gdp=read.csv('../data/Source Data/state_gdp_yoy_percent_change.csv') %>% filter(.$Geography != 'United States')
+state_gdp=read.csv('../../data/Source Data/state_gdp_yoy_percent_change.csv') %>% filter(.$Geography != 'United States')
 weights=c(1,1,1,1,1,1,1,2,3,4)
 state_gdp_2020 = state_gdp %>% mutate('gdp_change'=apply(state_gdp %>% select(c('X2018.Q1','X2018.Q2','X2018.Q3','X2018.Q4','X2019.Q1','X2019.Q2','X2019.Q3','X2019.Q4', 'X2020.Q1', 'X2020.Q2')),
                                                     1,weighted.mean,weights), 'year'=2020)
@@ -17,7 +17,7 @@ state_gdp_2016 = state_gdp %>% mutate('gdp_change'=apply(state_gdp %>% select(c(
                                                     1,weighted.mean,weights), 'year'=2016)
 gdp_changes=rbind(state_gdp_2016,state_gdp_2020)[,c('Geography','year','gdp_change')]
 
-state_gdp=read.csv('../data/Source Data/state_gdp_raw_in_millions.csv') %>% filter(.$Geography != 'United States')
+state_gdp=read.csv('../../data/Source Data/state_gdp_raw_in_millions.csv') %>% filter(.$Geography != 'United States')
 state_gdp_2020 = state_gdp %>% mutate('raw_gdp'=apply(state_gdp %>% select(c('X2018.Q1','X2018.Q2','X2018.Q3','X2018.Q4','X2019.Q1','X2019.Q2','X2019.Q3','X2019.Q4', 'X2020.Q1', 'X2020.Q2')),
                                                     1,weighted.mean,weights), 'year'=2020)
 state_gdp_2016 = state_gdp %>% mutate('raw_gdp'=apply(state_gdp %>% select(c('X2014.Q1','X2014.Q2','X2014.Q3','X2014.Q4','X2015.Q1','X2015.Q2','X2015.Q3','X2015.Q4', 'X2016.Q1', 'X2016.Q2')),
@@ -26,6 +26,6 @@ raw_gdp=rbind(state_gdp_2016,state_gdp_2020)[,c('Geography','year','raw_gdp')]
 
 state_gdp=left_join(gdp_changes,raw_gdp,by=c('Geography','year'))
 
-write.csv(state_gdp, file='../data/Clean Data/state_gdp.csv', sep=",", row.names=FALSE)
+write.csv(state_gdp, file='../../data/Clean Data/state_gdp.csv', sep=",", row.names=FALSE)
 
 #join state_gdp with the county names by state name. Then make a column that multiplies the number with pop/total pop
--- scripts/cleaning_scripts/unemployment_cleaner.R
+++ scripts/cleaning_scripts/unemployment_cleaner.R
@@ -8,7 +8,7 @@
 
 library(tidyverse)
 
-state_unemployment=read.csv('../data/Source Data/unemployment_rate_raw.csv') %>% filter(.$Geography != 'United States')
+state_unemployment=read.csv('../../data/Source Data/unemployment_rate_raw.csv') %>% filter(.$Geography != 'United States')
 weights=c(1,1,1,1,1,1,1,1,1,2,2,2,3,3,3,4,4,4)
 first="01"
 months=c('01','02','03','04','05','06','07','08','09','10','11','12')
@@ -23,4 +23,4 @@ state_unemployment = state_unemployment %>% mutate('2020'=apply(state_unemployme
 state_unemployment = state_unemployment %>% mutate('2016'=apply(state_unemployment %>% select(head_16),1,weighted.mean,weights))
 state_unemployment=state_unemployment %>% select(c('Geography','2020','2016')) %>% pivot_longer(!'Geography','Year','Unemployment')
 
-write.csv(state_unemployment,file='../data/Clean Data/state_unemployment.csv', sep=",", row.names=FALSE)
+write.csv(state_unemployment,file='../../data/Clean Data/state_unemployment.csv', sep=",", row.names=FALSE)
