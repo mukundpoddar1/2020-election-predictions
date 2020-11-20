c03b69405eb816d6a79dd0fc6050fa40aa96844a
saulholding <saulholding8@gmail.com>
2020-11-19 21:24:15 -0400

    unchanged

--- scripts/polling_data_pres_train.R
+++ scripts/polling_data_pres_train.R
@@ -38,7 +38,7 @@ df_merge <- read_csv("data/Clean Data/merged_final_dataset.csv")
 
 # Adding county state key for later merging
 df_merge <- df_merge %>%
-    mutate(counties_merge = paste(df_merge$stname.x, df_merge$ctyname.x, sep = " ")) 
+    mutate(counties_merge = paste(df_merge$ctyname.x, df_merge$stname.x, sep = ",")) 
     
 # Merging
 df_merged <- merge(df_merge, df_polls_state, by.x = 'stname.x', by.y = 'state', all.x = TRUE )
