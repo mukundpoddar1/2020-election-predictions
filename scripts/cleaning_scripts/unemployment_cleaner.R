fb7e359ebe7852242e92d516027d735b212bb3b7
Mukund Poddar <mukundpoddar1@gmail.com>
2020-11-23 03:46:41 +0530

    Replicate state eunemployment rate for counties

--- scripts/cleaning_scripts/unemployment_cleaner.R
+++ scripts/cleaning_scripts/unemployment_cleaner.R
@@ -21,6 +21,16 @@ head_16=c(head_16,paste(paste0('X',"2016"),months[1:9],first, sep="."))
 
 state_unemployment = state_unemployment %>% mutate('2020'=apply(state_unemployment %>% select(head_20),1,weighted.mean,weights))
 state_unemployment = state_unemployment %>% mutate('2016'=apply(state_unemployment %>% select(head_16),1,weighted.mean,weights))
-state_unemployment=state_unemployment %>% select(c('Geography','2020','2016')) %>% pivot_longer(!'Geography','Year','Unemployment')
+state_unemployment=state_unemployment %>% select(c('Geography','2020','2016')) %>% pivot_longer(!'Geography','year',values_to='unemployment')
 
-write.csv(state_unemployment,file='../../data/Clean Data/state_unemployment.csv', sep=",", row.names=FALSE)
+#The unemployment rate needs to be brought to a county level. Because we do not have 
+# data on how different counties might have different circumstances, we are replicating
+# the unemployment rate across the counties of a state
+
+fips_codes = read.csv('../../data/Clean Data/base_county_state_fips_lkp.csv') %>% select('stname', 'fips')
+county_unemployment = left_join(state_unemployment, fips_codes, by=c('Geography'='stname'))
+
+write.csv(county_unemployment %>% filter(year==2016) %>% select(fips, unemployment), 
+          file='../../data/Clean Data/unemployment_2016.csv', sep=",", row.names=FALSE)
+write.csv(county_unemployment %>% filter(year==2020) %>% select(fips, unemployment), 
+          file='../../data/Clean Data/unemployment_2020.csv', sep=",", row.names=FALSE)
